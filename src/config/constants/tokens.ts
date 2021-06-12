63145a522de8fbf6cf31b528be86d4a6ffe3d706
AngDeFi <84949712+AngDeFi@users.noreply.github.com>
2021-06-12 21:21:55 +0700

    Update tokens.ts

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -7,7 +7,7 @@ const tokens = {
     symbol: 'CAKE',
     address: {
       137: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
-      97: '0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe',
+      56: '0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe',
     },
     decimals: 18,
     projectLink: 'https://pancakeswap.finance/',
