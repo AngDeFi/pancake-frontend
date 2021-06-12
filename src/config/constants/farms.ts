53b01a635a298e59a98bfe7633341f182001c076
AngDeFi <84949712+AngDeFi@users.noreply.github.com>
2021-06-12 21:53:04 +0700

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -5,16 +5,7 @@ const farms: FarmConfig[] = [
   /**
    * These 3 farms (PID 0, 251, 252) should always be at the top of the file.
    */
-  {
-    pid: 0,
-    lpSymbol: 'CAKE',
-    lpAddresses: {
-      97: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
-      137: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
-    },
-    token: tokens.syrup,
-    quoteToken: tokens.wbnb,
-  },
+ 
   {
     pid: 251,
     lpSymbol: 'CAKE-BNB LP',
