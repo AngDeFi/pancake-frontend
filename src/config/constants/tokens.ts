43efc52b8bba013d940b6d6670e903d126aa9ea9
AngDeFi <84949712+AngDeFi@users.noreply.github.com>
2021-06-12 21:59:34 +0700

    Update tokens.ts

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -6,7 +6,7 @@ const tokens = {
   cake: {
     symbol: 'CAKE',
     address: {
-      97: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
+      80001: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
       137: '0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe',
     },
     decimals: 18,
@@ -15,7 +15,7 @@ const tokens = {
   busd: {
     symbol: 'BUSD',
     address: {
-      97: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
+      80001: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
       137: '0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe',
     },
     decimals: 18,
@@ -24,7 +24,7 @@ const tokens = {
   usdt: {
     symbol: 'USDT',
     address: {
-      97: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
+      80001: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
       137: '0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe',
     },
     decimals: 18,
