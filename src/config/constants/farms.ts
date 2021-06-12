cf1503dcedcf0837b3f79b05d28daf0fbe499dea
AngDeFi <84949712+AngDeFi@users.noreply.github.com>
2021-06-12 23:22:02 +0700

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -11,19 +11,19 @@ const farms: FarmConfig[] = [
     lpSymbol: 'CAKE-MATIC LP',
     lpAddresses: {
       80001: '0x3ed8936cAFDF85cfDBa29Fbe5940A5b0524824F4',
-      137: '0x3ed8936cAFDF85cfDBa29Fbe5940A5b0524824F4',
+      137: '0x4d02bf5869fb3cfea155710d637d79afadb2cc8d',
     },
     token: tokens.cake,
     quoteToken: tokens.wmatic,
   },
   {
     pid: 2,
-    lpSymbol: 'USDT-MATIC LP',
+    lpSymbol: 'USDC-MATIC LP',
     lpAddresses: {
       80001: '0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16',
-      137: '0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16',
+      137: '0x6e7a5FAFcec6BB1e78bAE2A1F0B612012BF14827',
     },
-    token: tokens.usdt,
+    token: tokens.usdc,
     quoteToken: tokens.wmatic,
   },
   /**
