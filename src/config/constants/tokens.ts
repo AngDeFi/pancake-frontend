1ae24b6d8f3c86d15a53c9c8f9f326316f6d01b8
AngDeFi <84949712+AngDeFi@users.noreply.github.com>
2021-06-12 21:11:06 +0700

    Update tokens.ts

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -1,12 +1,12 @@
 const tokens = {
   bnb: {
-    symbol: 'BNB',
-    projectLink: 'https://www.binance.com/',
+    symbol: 'MATIC',
+    projectLink: 'https://polygon.technology/',
   },
   cake: {
     symbol: 'CAKE',
     address: {
-      56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
+      137: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
       97: '0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe',
     },
     decimals: 18,
@@ -438,7 +438,7 @@ const tokens = {
   wbnb: {
     symbol: 'wBNB',
     address: {
-      56: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
+      137: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
       97: '0xae13d989dac2f0debff460ac112a837c89baa7cd',
     },
     decimals: 18,
