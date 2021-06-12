5092a63f98b324152e80226bc02bf126f4b0bf4f
AngDeFi <84949712+AngDeFi@users.noreply.github.com>
2021-06-12 23:09:46 +0700

    Update tokens.ts

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -7,7 +7,7 @@ const tokens = {
     symbol: 'CAKE',
     address: {
       80001: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
-      137: '0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe',
+      137: '0x805262b407177c3a4aa088088c571164f645c5d0',
     },
     decimals: 18,
     projectLink: 'https://pancakeswap.finance/',
@@ -16,10 +16,10 @@ const tokens = {
     symbol: 'WBNB',
     address: {
       80001: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
-      137: '0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe',
+      137: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
     },
     decimals: 18,
-    projectLink: 'https://pancakeswap.finance/',
+    projectLink: 'https://polygon.technology/',
   },
   busd: {
     symbol: 'BUSD',
@@ -34,10 +34,10 @@ const tokens = {
     symbol: 'USDT',
     address: {
       80001: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
-      137: '0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe',
+      137: '0xc2132d05d31c914a87c6611c10748aeb04b58e8f',
     },
     decimals: 18,
-    projectLink: 'https://pancakeswap.finance/',
+    projectLink: 'https://tether.to/',
   },
    kalm: {
     symbol: 'KALM',
@@ -48,7 +48,15 @@ const tokens = {
     decimals: 18,
     projectLink: 'https://pancakeswap.finance/',
   },
-  
+  syrup: {
+    symbol: 'SYRUP',
+    address: {
+      80001: '0x1bc18e1a9D7De2c73B0FF3944A538463E7B04E65',
+      137: '0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe',
+    },
+    decimals: 18,
+    projectLink: 'https://pancakeswap.finance/',
+  },
 }
 
 export default tokens
