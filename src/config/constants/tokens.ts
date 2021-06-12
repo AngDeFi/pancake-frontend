945c5b07de5464880382c3dca792795f40bf6403
AngDeFi <84949712+AngDeFi@users.noreply.github.com>
2021-06-12 22:16:49 +0700

    Update tokens.ts

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -12,6 +12,15 @@ const tokens = {
     decimals: 18,
     projectLink: 'https://pancakeswap.finance/',
   },
+  wbnb: {
+    symbol: 'WBNB',
+    address: {
+      80001: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
+      137: '0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe',
+    },
+    decimals: 18,
+    projectLink: 'https://pancakeswap.finance/',
+  },
   busd: {
     symbol: 'BUSD',
     address: {
