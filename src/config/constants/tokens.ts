0a453a8f4e1547aaf69f311b1895ab627e2f9465
AngDeFi <84949712+AngDeFi@users.noreply.github.com>
2021-06-12 22:23:11 +0700

    Update tokens.ts

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -39,7 +39,15 @@ const tokens = {
     decimals: 18,
     projectLink: 'https://pancakeswap.finance/',
   },
-  
+   kalm: {
+    symbol: 'KALM',
+    address: {
+      80001: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
+      137: '0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe',
+    },
+    decimals: 18,
+    projectLink: 'https://pancakeswap.finance/',
+  },
   
 }
 
